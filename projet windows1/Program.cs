42c6905546c473f5449ea144c00e8b7445167c2f
simonlac <101816652+simonlac@users.noreply.github.com>
2022-10-26 10:28:23 -0400

    Add files via upload

---
+++ projet windows1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetwin
+{
+     class Program
+    {
+        
+        public static int somme(int a, int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+           
+            
+            Console.WriteLine("Bonjour");
+            Console.WriteLine(somme(15, 20));
+            Console.WriteLine(somme(15, 39));
+            Console.ReadLine();
+        } 
+
+     }
+}
+
